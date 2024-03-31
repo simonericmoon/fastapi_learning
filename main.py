98e3ac206a0d9e5a139504432a0af2431147776d
simonericmoon <simon.dunheim@gmail.com>
2024-03-31 11:25:55 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+products = []
+
+@app.get("/")
+async def get_products():
+    return products
+
+@app.get("/products/{product_id}") #pathparameter
+async def get_product(product_id: int):
+    for product in products:
+        if product.get("id") == product_id:
+            return product
+    return {"error": "Product not found"}
+
+@app.put("/products/{product_id}")
+async def update_product(product_id: int, product: dict):
+    for index, p in enumerate(products):
+        if p.get("id") == product_id:
+            products[index] = product
+            return {"sucess": "product updated successfully"}
+    return {"error": "Product not found"}   
+
+@app.delete("/products/{product_id}")
+async def delete_product(product_id: int):
+    for index, p in enumerate(products):
+        if p.get("id") == product_id:
+            products.pop(index)
+            return {"sucess": "product deleted successfully"}
+    return {"error": "Product not found"}
+
+#if __name__ == "__main__": 
+#    uvicorn.run(app, host="127.0.0.1", port=4444) # also runnable with uvicorn main:app --port 4444 --reload
+
+@app.post("/products")
+async def create_product(product: dict):
+    products.append(product)
+    return {"sucess": "product added successfully"}
